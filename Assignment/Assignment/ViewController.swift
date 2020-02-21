d34fcd960be55578b45de8df3e79fd2267b84a97
Rajmani Kushwaha <rajmanikush@gmail.com>
2020-02-21 20:22:14 +0530

    project created

---
+++ Assignment/Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Rajmani Kushwaha on 21/02/20.
+//  Copyright © 2020 Rajmani Kushwaha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
